2dd9a8e5546ce7bb093ca03d81f186958b9186ff
semihcoskunn <coskunsemih23@iu.edu.tr>
2025-05-28 20:13:53 +0300

    u1

--- script.js
+++ script.js
@@ -2,26 +2,40 @@ const text1 = "Web Sitesine";
 const text2 = "Hoşgeldiniz"
 
 let i = 0;
-let j= 0;
-const speed =50;
+let j = 0;
+const speed = 50;
 
 function typeWriter1() {
-    if(i < text1.length) {
+    if (i < text1.length) {
         document.getElementById("text1").innerHTML += text1.charAt(i);
         i++;
-        setTimeout(typeWriter1,speed);
+        setTimeout(typeWriter1, speed);
     }
-    else{
-        setTimeout(typeWriter2,200);
+    else {
+        setTimeout(typeWriter2, 200);
     }
 }
 function typeWriter2() {
-    if(j< text2.length) {
+    if (j < text2.length) {
         document.getElementById("text2").innerHTML += text2.charAt(j);
         j++;
-        setTimeout(typeWriter2,speed);
+        setTimeout(typeWriter2, speed);
     }
 }
-window.onload =typeWriter1
+window.onload = typeWriter1
 
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".contact-form");
+    const successMessage = document.getElementById("successMessage");
 
+    form.addEventListener("submit", (e) => {
+        e.preventDefault(); // sayfa yenilenmesin
+
+        successMessage.style.display = "block";
+
+        setTimeout(() => {
+            successMessage.style.display = "none";
+            form.reset(); // formu temizle
+        }, 3000);
+    });
+});
